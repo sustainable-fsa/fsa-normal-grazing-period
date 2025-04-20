47b0879426c2be39c3303ec1df8c448ea371c987
bocinsky <bocinsky@gmail.com>
2025-04-20 07:54:02 -0600

    do dashboards work

--- fsa-normal-grazing-period.R
+++ fsa-normal-grazing-period.R
@@ -3,40 +3,6 @@ library(magrittr)
 library(khroma)
 library(patchwork)
 
-## The FSA county definitions 
-fsa_counties <-
-  sf::read_sf("/vsizip/../fsa-lfp-eligibility/fsa-counties/FSA_Counties_dd17.gdb.zip") %>%
-  dplyr::select(`FSA Code` = FSA_STCOU,
-                `State Name` = STATENAME,
-                `County Name` = FSA_Name) %>%
-  {
-    # Round-trip to geojson to get rid of strange curved geometry
-    tmp <- tempfile(fileext = ".geojson")
-    sf::write_sf(., tmp,
-                 delete_dsn = TRUE)
-    sf::read_sf(tmp)
-  } %>%
-  dplyr::group_by(`FSA Code`, `State Name`, `County Name`) %>%
-  dplyr::summarise(.groups = "drop") %>%
-  sf::st_transform("EPSG:5070") %>%
-  sf::st_make_valid() %>%
-  sf::st_intersection(  
-    tigris::counties(cb = TRUE) %>%
-      sf::st_union() %>%
-      sf::st_transform("EPSG:5070")
-  ) %>%
-  rmapshaper::ms_simplify(keep = 0.015) %>%
-  sf::st_make_valid() %>%
-  sf::st_transform("OGC:CRS84") %>%
-  sf::st_make_valid()
-
-
-%>%
-  tigris::shift_geometry() %>%
-  dplyr::filter(!(FSA_STATE %in% c("14-GU", "52-VI", "60-AS", "69-MP"))) %>%
-  sf::write_sf("fsa-counties.geojson",
-               delete_dsn = TRUE)
-
 # FSA-defined Normal Grazing Periods (2022)
 fsa_normal_grazing_period <-
   unzip(zipfile = "foia/2025-FSA-04691-F Bocinsky.zip",
@@ -87,7 +53,7 @@ fsa_normal_grazing_period <-
                 `Normal Grazing Period End Date` = 
                   case_when(`Program Year` == 2016 & `FSA Code` == "30079" ~ lubridate::as_date("2016-12-01"),
                             .default = `Normal Grazing Period End Date`)
-                ) %>%
+  ) %>%
   dplyr::select(!`New Code`) %>%
   dplyr::filter(
     !(`County Name` %in% 
@@ -98,6 +64,19 @@ fsa_normal_grazing_period <-
   ) %>%
   dplyr::distinct() %T>%
   readr::write_csv("fsa-normal-grazing-period.csv")
+
+## The FSA county definitions 
+fsa_counties <-
+  sf::read_sf("https://climate-smart-usda.github.io/fsa-counties-dd17/fsa-counties-dd17.geojson") %>%
+  dplyr::filter(!(`State Name` %in% 
+                    c("American Samoa", 
+                      "Northern Mariana Islands", 
+                      "Guam",
+                      "Virgin Islands of the U.S."))) %>%
+  tigris::shift_geometry() %T>%
+  geojsonio::topojson_write(file = "fsa_counties.topojson",
+                            convert_wgs84 = FALSE,
+                            overwrite = TRUE)
 
 ## yday cyclic palette
 dates <- seq(lubridate::as_date("1999-07-01"), lubridate::as_date("2000-06-30"), "1 day")
@@ -166,7 +145,7 @@ lfp_growing_seasons <-
       (
         
         dplyr::left_join(fsa_counties, data,
-                         by = c("FSA_CODE" = "FSA Code")) %>%
+                         by = c("FSA Code" = "FSA Code")) %>%
           tidyr::pivot_longer(`Start Date`:`End Date`) %>%
           dplyr::mutate(name = factor(name,
                                       levels = c("Start Date",
@@ -179,7 +158,7 @@ lfp_growing_seasons <-
           geom_sf(aes(fill = color),
                   col = "white") +
           geom_sf(data = fsa_counties %>%
-                    dplyr::group_by(FSA_STATE) %>%
+                    dplyr::group_by(`State Name`) %>%
                     dplyr::summarise(),
                   col = "white",
                   fill = NA,
@@ -218,44 +197,26 @@ lfp_growing_seasons$graph
 dev.off()
 
 
-## ISSUES
-
-known_issues <-fsa_normal_grazing_period %>%
-  dplyr::select(`Program Year`, 
-                `FSA Code`,
-                `Pasture Type`,
-                `Normal Grazing Period Start Date`,
-                `Normal Grazing Period End Date`) %>%
-  {
-    dplyr::left_join(
-      dplyr::select(., `Program Year`, 
-                    `FSA Code`,
-                    `Pasture Type`) %>%
-        {dplyr::filter(., duplicated(.))},
-      .
-    )
-  } %>%
-  dplyr::right_join(fsa_normal_grazing_period, .) %>%
-  dplyr::arrange(`Program Year`, 
-                 `FSA Code`,
-                 `Pasture Type`,
-                 `State Name`, `County Name`) %>%
-  distinct() %T>%
-  readr::write_csv("2025-FSA-04691-F Bocinsky.known_issues.csv")
-
-
-
-# odd_start_records <-
-  fsa_normal_grazing_period %>%
-  dplyr::filter(!(lubridate::mday(`Normal Grazing Period Start Date`) %in% c(1,15))) %>%
-    nrow()
-  # 25285 records
-
-# odd_end_records <-
-  fsa_normal_grazing_period %>%
-  dplyr::filter(!(
-    (lubridate::mday(`Normal Grazing Period End Date`) %in% c(1,15)) |
-      (lubridate::mday(`Normal Grazing Period End Date`) == lubridate::days_in_month(`Normal Grazing Period End Date`))
-    )) %>%
-  nrow()
-  # 30490 records
+# # ISSUES
+# known_issues <-fsa_normal_grazing_period %>%
+#   dplyr::select(`Program Year`, 
+#                 `FSA Code`,
+#                 `Pasture Type`,
+#                 `Normal Grazing Period Start Date`,
+#                 `Normal Grazing Period End Date`) %>%
+#   {
+#     dplyr::left_join(
+#       dplyr::select(., `Program Year`, 
+#                     `FSA Code`,
+#                     `Pasture Type`) %>%
+#         {dplyr::filter(., duplicated(.))},
+#       .
+#     )
+#   } %>%
+#   dplyr::right_join(fsa_normal_grazing_period, .) %>%
+#   dplyr::arrange(`Program Year`, 
+#                  `FSA Code`,
+#                  `Pasture Type`,
+#                  `State Name`, `County Name`) %>%
+#   distinct() %T>%
+#   readr::write_csv("2025-FSA-04691-F Bocinsky.known_issues.csv")
